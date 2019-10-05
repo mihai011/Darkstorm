9a8fee5a85f8d22fef95868dfdc38b9cf08204f2
mihai011 <mihaimihai011>
2019-10-05 10:56:03 +0300

    Creating functions.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import json
 
 import boto3
 import botocore
+import logging
+
+from botocore.exceptions import ClientError
 
 def read_configuration(path):
 
@@ -31,15 +34,25 @@ def read_lambda_conf(path):
     return data["lambda_conf"]
 
 
+def create_lambda(client,function_name, conf):
+
+    try:
+        client.create_lambda(**conf)
+    except ClientError as e:
+        logging.error(e)
+
 def main(configuration_path, lambda_conf_path):
 
     connections, functions = read_configuration("conf.json")
     lambda_conf = read_lambda_conf("lambda_conf.json")
 
     #create lambda functions 
-
+    client = boto3.client('lambda')
     for function_name in functions:
-        
+        create_lambda(client, function_name, lambda_conf)
+    
+    logging.info("Functions created!")
 
+    
 
     
