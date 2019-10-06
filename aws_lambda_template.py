5c7b5d1a4959745188e374e1203075c433c08177
mihai011 <mihaimihai011>
2019-10-06 17:12:11 +0300

    Added function communication

--- aws_lambda_template.py
+++ aws_lambda_template.py
@@ -1,7 +1,18 @@
 import boto3
 import logging
+import json
 
 def handler(event, context):
-    pass
+    
+    #get connections
+    with open("connection_file.json") as f:
+        connections = json.loads(f.read())
 
+    connections = connections["connections"]
+
+    client = boto3.client('lambda')
+
+    for function in connections:
+        client.invoke(FunctionName=function,\
+        InvocationType="Event")
 
--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import boto3
 import botocore
 import logging
 
-
 from botocore.exceptions import ClientError
+
+def show_banner(file):
+
+    with open(file) as f:
+        print(f.read())
 
 def read_configuration(path):
 
@@ -15,8 +19,8 @@ def read_configuration(path):
         data = json.loads(f.read())
 
     #get functions names
-
     connections = data["configuration"]
+    trigger_function = data["trigger_function"]
 
     functions_send = list(connections.keys())
 
@@ -27,7 +31,7 @@ def read_configuration(path):
     
     functions = set(functions_send + all_functions_recv)
 
-    return connections, functions
+    return connections, functions, trigger_function
 
 def read_lambda_conf(path):
 
@@ -38,11 +42,22 @@ def read_lambda_conf(path):
 
 def precreate_lambda(template, function_name, connections):
 
-    zip = zipfile.ZipFile(function_name + ".zip",'a')
+    archive_name = "{}.zip".format(function_name)
+    connection_file = "connection_file.json"
+
+    if os.path.exists(archive_name):
+        os.remove(archive_name)
+
+    if os.path.exists(connection_file):
+        os.remove(connection_file)
+
+    zip = zipfile.ZipFile(archive_name,'a')
     zip.write(template,"main.py")
-    connection_file = "connection_{}.json".format(function_name)
+    
     with open(connection_file, 'w+') as f:
-        f.write(str(connections[function_name]))
+        cell = {}
+        cell["connections"] = connections[function_name]
+        f.write(json.dumps(cell))
     zip.write(connection_file, os.path.basename(connection_file))
     zip.close()
 
@@ -60,16 +75,24 @@ def create_lambda(client, function_name, conf):
     conf["Code"] = {"ZipFile":archive}
 
     try:
-        client.create_function(**conf)
+        res = client.create_function(**conf)
+        logging.info(res)
     except ClientError as e:
         logging.error(e)
 
+
+def call_function(client, function_name, event):
+    
+    res = client.invoke(FunctionName=function_name,\
+         InvocationType = "Event")
+    logging.info(res)
+
 def main(template_path, configuration_path, lambda_conf_path):
 
-    connections, functions = read_configuration("conf.json")
+    connections, functions, trigger_function = read_configuration("conf.json")
     lambda_conf = read_lambda_conf("lambda_conf.json")
+    print("Configurations read!")
 
-   
     #deletes previous versions and create lambda functions 
     client = boto3.client('lambda')
     for function_name in functions:
@@ -77,10 +100,19 @@ def main(template_path, configuration_path, lambda_conf_path):
         precreate_lambda(template_path, function_name, connections)
         create_lambda(client, function_name, lambda_conf)
     
+    print("Functions created!")
     logging.info("Functions created!")
 
+    #invoke lambda
+    event = {}
+    call_function(client, trigger_function, event)
+
+    print("Trigger function called!")
+    logging.info("Trigger function called!")
     
 if __name__ == "__main__":
+
+    show_banner("banner.txt")
 
     template_path = "aws_lambda_template.py"
     configuration_path = "conf.json"
